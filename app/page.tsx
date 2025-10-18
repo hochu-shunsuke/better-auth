23d63ac57bd851fcd8b5c8b9d3bdf7a24c57ddbe
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2025-10-19 02:23:50 +0900

    リポジトリリンク追加

--- app/page.tsx
+++ app/page.tsx
@@ -86,6 +86,9 @@ export default async function Home() {
 						<SignInButton />
 					</Suspense>
 				</div>
+				<a href="https://github.com/hochu-shunsuke/better-auth" target="_blank" rel="noopener noreferrer">
+						<img src="https://gh-card.dev/repos/hochu-shunsuke/better-auth.svg?fullname=" alt="GitHub Repo Card" style={{ display: "block", margin: "0 auto" }} width={400} />
+				</a>
 			</main>
 		</div>
 	);
