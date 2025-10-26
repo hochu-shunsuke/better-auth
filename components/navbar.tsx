a795779b86b9a2e23335c3cf44bdcb79a28e927b
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2025-10-26 23:19:49 +0900

    navbarをssrにした，（ログイン状態表示までのロードいらないから）

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,30 +1,11 @@
-"use client";
-
 import Link from "next/link";
+import { headers } from "next/headers";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "@/components/ui/button";
-import { client } from "@/lib/auth-client";
+import { auth } from "@/lib/auth";
 
-export default function Navbar() {
-  const { data: session, isPending } = client.useSession();
-
-  if (isPending) {
-    return (
-      <div className="bg-white dark:bg-black border-b py-2 fixed top-0 left-0 z-50 w-full">
-        <div className="flex justify-between items-center border-border mx-auto w-full max-w-screen-lg px-4 md:px-1">
-          <Link href="/">
-            <div className="flex gap-2 cursor-pointer">
-              <p className="dark:text-white text-black">My App</p>
-            </div>
-          </Link>
-          <div className="z-50 flex items-center gap-2">
-            <ThemeToggle />
-            <div className="w-20 h-8 bg-gray-200 animate-pulse rounded"></div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+export default async function Navbar() {
+  const session = await auth.api.getSession({ headers: await headers() });
 
   return (
     <div className="bg-white dark:bg-black border-b py-2 fixed top-0 left-0 z-50 w-full">
